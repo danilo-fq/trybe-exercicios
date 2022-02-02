f05f88d441141f9ab87a6d3d67402f72b416d705
Danilo <danilof.queiroz1@gmail.com>
2022-02-02 18:25:23 -0300

    adiciona exercicio 1

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/p1.js
@@ -0,0 +1,17 @@
+const a = 5;
+const b = 9;
+
+let sum = a + b;
+console.log(sum);
+
+let sub = a - b;
+console.log(sub);
+
+let mult = a * b;
+console.log(mult);
+
+let div = a / b;
+console.log(div);
+
+let mod = a % b;
+console.log(mod);
