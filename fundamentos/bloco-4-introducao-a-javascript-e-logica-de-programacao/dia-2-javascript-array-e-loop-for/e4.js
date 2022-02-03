1a5956c1c93f673901de4076f2c83a12fb2d3176
Danilo <danilof.queiroz1@gmail.com>
2022-02-03 18:19:57 -0300

    Exercicio 4 feito

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/e4.js
@@ -0,0 +1,18 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+//Exercicio 4 - Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+
+let sum = 0;
+
+for(let i = 0; i < numbers.length; i+=1){
+  sum+= numbers[i];
+}
+let media = sum/numbers.length;
+
+console.log(media);
+
+if (media > 20) {
+  console.log("valor maior que 20");
+} else {
+  console.log("valor menor ou igual a 20");
+}
