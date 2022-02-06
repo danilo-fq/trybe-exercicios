6ad0679309555b6ebaa2149fddf9f5d77260c0bb
Danilo <danilof.queiroz1@gmail.com>
2022-02-06 15:45:10 -0300

    Exercicio bonus 2 - feito

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/b2.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/b2.js
@@ -2,3 +2,13 @@
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
+for (let foward = 1; foward < numbers.length; foward +=1 ) {
+  for (let before = 0; before < foward; before += 1) {
+    if (numbers[foward] > numbers[before]) {
+      let beLast = numbers[before];
+      numbers[before] = numbers[foward];
+      numbers[foward] = beLast;
+    }
+  }
+}
+console.log(numbers);
