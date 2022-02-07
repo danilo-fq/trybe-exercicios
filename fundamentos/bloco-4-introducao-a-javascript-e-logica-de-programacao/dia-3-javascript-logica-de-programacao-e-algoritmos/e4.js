c348ab65ccc78fef8c6f4193e229bb3600c6f17d
Danilo <danilof.queiroz1@gmail.com>
2022-02-07 10:17:23 -0300

    Exercicio 4 feito

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/e4.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/e4.js
@@ -1,4 +1,16 @@
-//4 - Um número primo é aquele divisível apenas por 1 e por ele mesmo. Sabendo disso, escreva um algoritmo que retorne o maior número primo entre 0 e 50.
+//4 - Um número primo é aquele divisível apenas por 1 e por ele mesmo. 
+// Sabendo disso, escreva um algoritmo que retorne o maior número primo entre 0 e 50.
+let biggestPrime = 2;
+for(let prime = 3; prime <= 50; prime+=1){
+  let isNotPrime = 0;
+  for(let divider = 2; divider < prime; divider+=1){
+    if(prime%divider == 0){
+      isNotPrime += 1;
 
-
-
+    }
+}
+  if (isNotPrime === 0) {
+    biggestPrime = prime;
+  }
+}
+console.log(biggestPrime);
