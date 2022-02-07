6d7cdbb0964240983fe41abbc827f95035149ca7
Danilo <danilof.queiroz1@gmail.com>
2022-02-06 21:36:46 -0300

    Exercicio 3 feito

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/e3.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/e3.js
@@ -1,3 +1,22 @@
 // 3 - Escreva dois algoritmos: um que retorne a maior palavra deste array e outro que retorne a menor. Considere o número de caracteres de cada palavra.
 
 let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+let longestWord = '';
+let smallerWord = '';
+
+
+
+for (let index = 0; index < array.length; index +=1) {
+  if (array[index].length > longestWord.length) {
+    longestWord = array[index];
+  }
+}
+
+for (let index = 0; index < array.length; index +=1) {
+  if (array[index].length < longestWord.length && array[index].length !== 0) {
+    smallerWord = array[index];
+  }
+}
+console.log("Palavra mais longa", longestWord);
+console.log("Palavra mais curta", smallerWord);
