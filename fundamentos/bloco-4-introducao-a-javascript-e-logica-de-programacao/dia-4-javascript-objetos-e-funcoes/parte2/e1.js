f7162875983ce9ce03b73ce9deaf21389da59327
Danilo <danilof.queiroz1@gmail.com>
2022-02-08 11:36:31 -0300

    exercicio functions 1 feito

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/parte2/e1.js
@@ -0,0 +1,24 @@
+//usando split, join e reverse
+function isPalindrome(word) {
+  let reverseWord = word.split('').reverse().join('');
+  if (reverseWord === word) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+//usando for
+
+function isPalindrome(word) {
+  let reverseWord = '';
+  for (let index = word.length-1; index >= 0; index-=1) {
+    reverseWord+= word[index];
+  }
+  if (reverseWord === word) {
+    return true;
+  } else {
+    return false;
+  }
+};
+console.log(isPalindrome('arara'));
