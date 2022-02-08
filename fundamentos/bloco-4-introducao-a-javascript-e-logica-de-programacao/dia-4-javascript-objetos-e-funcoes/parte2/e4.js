05db6ad567ec81ac2d201596d2ec19ac5d127038
Danilo <danilof.queiroz1@gmail.com>
2022-02-08 13:47:22 -0300

    Functions: exercicio 4 feito

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/parte2/e4.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/parte2/e4.js
@@ -1,3 +1,14 @@
 //4 - Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
 
 let namesArray = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
+
+function biggestName (names) {
+  let isTheBiggestName = '';
+
+  for (const value of names) {
+    if (value.length > isTheBiggestName.length) {
+      isTheBiggestName = value;
+    }
+  }
+  return isTheBiggestName + ', Quantidade de caracteres: ' + isTheBiggestName.length;
+  };
