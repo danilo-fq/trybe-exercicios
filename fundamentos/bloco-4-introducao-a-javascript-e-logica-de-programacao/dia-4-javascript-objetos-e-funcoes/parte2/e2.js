c4407e9d81c6ef488898bc91b45e4554a48b89fa
Danilo <danilof.queiroz1@gmail.com>
2022-02-08 13:32:36 -0300

    exercicio functions 2 feito

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/parte2/e2.js
@@ -0,0 +1,11 @@
+let numbersArray = [1, 50, 74, 23, 120, 33, 35, 42, 200];
+
+function isBiggestNumber (numbers) {
+  let biggestNumber = 0;
+  for (let index = 0; index < numbers.length; index+=1) {
+    if (numbers[index] > biggestNumber) {
+      biggestNumber = numbers[index];
+    }
+  }
+  return biggestNumber;
+}
