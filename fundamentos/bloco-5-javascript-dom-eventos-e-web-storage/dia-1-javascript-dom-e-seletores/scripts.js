67910328a2a4160d99eb562de794a74610c05bbb
Danilo <danilof.queiroz1@gmail.com>
2022-02-09 19:22:03 -0300

    cabecalho das questoes organizados

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/scripts.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/scripts.js
@@ -1,17 +1,12 @@
-/*
+        /*
         Aqui você vai modificar os elementos já existentes utilizando apenas as funções:
         - document.getElementById()
         - document.getElementsByClassName()
         - document.getElementsByTagName()
- 1 - Crie uma função que mude o texto na tag <p> para uma descrição de como você se vê daqui a 2 anos. (Não gaste tempo pensando no texto e sim realizando o exercício)
- 2 - Crie uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
- 3 - Crie uma função que mude a cor do quadrado vermelho para branco.
- 4 - Crie uma função que corrija o texto da tag <h1>.
- 5 - Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
- 6 - Crie uma função que exiba o conteúdo de todas as tags <p> no console.
+
         */
 
-// Exercicio 1
+// Exercicio 1 - Crie uma função que mude o texto na tag <p> para uma descrição de como você se vê daqui a 2 anos. (Não gaste tempo pensando no texto e sim realizando o exercício)
 
 function changeText() {
   let paragraph = document.getElementsByTagName('p');
@@ -20,7 +15,7 @@ function changeText() {
 
 changeText();
 
-// Exercicio 2
+// Exercicio 2 - Crie uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
 
 function changeBgColor(className, color) {
   let mainDiv = document.getElementsByClassName(className);
@@ -28,11 +23,11 @@ function changeBgColor(className, color) {
 }
 changeBgColor('main-content', 'rgb(76,164,109)');
 
-// Exercicio 3
+// Exercicio 3 - Crie uma função que mude a cor do quadrado vermelho para branco.
 
 changeBgColor('center-content', 'White');
 
-// Exercicio 4
+// Exercicio 4 - Crie uma função que corrija o texto da tag <h1>.
 
 function changeTagText(tagName, text){
   let title = document.getElementsByTagName(tagName);
@@ -41,7 +36,7 @@ function changeTagText(tagName, text){
 
 changeTagText('h1', 'Exercício 5.1 - JavaScript');
 
-// Exercicio 5
+// Exercicio 5 - Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
 
 function toUpperCaseTagText(tagName) {
   let paragraphs = document.getElementsByTagName(tagName);
@@ -51,7 +46,7 @@ function toUpperCaseTagText(tagName) {
 }
 toUpperCaseTagText('p');
 
-// Exercicio 6
+// Exercicio 6 - Crie uma função que exiba o conteúdo de todas as tags <p> no console.
 
 function showOnConsole(tagName) {
   let paragraphs = document.getElementsByTagName(tagName);
