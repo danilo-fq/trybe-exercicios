7905f993db60b89f9005e0da91260aa92ea39b41
Danilo <danilof.queiroz1@gmail.com>
2022-02-10 19:33:03 -0300

    exercicio 5 feito

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -41,6 +41,7 @@ document.getElementsByTagName('section')[0].appendChild(newP);
 
 // 5 - Adicione a tag section com a classe left-content como filho da tag main criada no passo 2;
 
+document.getElementsByClassName('center-content')[0].className += " left-content";
 
 // 6 - Adicione a tag section com a classe right-content como filho da tag main criada no passo 2;
 
