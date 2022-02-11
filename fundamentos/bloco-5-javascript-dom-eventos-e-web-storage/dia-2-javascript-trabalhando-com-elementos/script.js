bf94a3c79a12f6aeaa5945446bab625c4ad6f147
Danilo <danilof.queiroz1@gmail.com>
2022-02-11 10:01:04 -0300

    Correcao dos exercicios 1 ao 7 criando as respectivas sections e nao adicionando nova classe a uma unica section

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -2,54 +2,82 @@
 // 1 - Adicione a tag h1 com o texto Exercício 5.2 - JavaScript DOM como filho da tag body ;
   
 // criar uma variavel e criar elemento h1
-  let newH1 = document.createElement("h1");
+  let elementH1 = document.createElement("h1");
 
   // atribuir h1 como filho do body
-  document.body.appendChild(newH1);
+  document.body.appendChild(elementH1);
 
 // 2 - Adicione a tag main com a classe main-content como filho da tag body ;
 
 // criar elemento main
-let newMain = document.createElement("main");
+let elementMain = document.createElement("main");
 
 //atribuir a classe para main
-newMain.className = 'main-content';
+elementMain.className = 'main-content';
 
 // por tag main como filho do body
-document.body.appendChild(newMain);
+document.body.appendChild(elementMain);
 
 
 // 3 - Adicione a tag section com a classe center-content como filho da tag main criada no passo 2;
 
 //criar o elemento section
-let newSection = document.createElement("section");
+let elementSectionCenter = document.createElement("section");
 
 //atribuir a classe para section
-newSection.className = 'center-content';
+elementSectionCenter.className = 'center-content';
 
 // por tag section como filho da tag main
-document.getElementsByTagName('main')[0].appendChild(newSection);
+document.getElementsByTagName('main')[0].appendChild(elementSectionCenter);
 
 
 // 4 - Adicione a tag p como filho do section criado no passo 3 e coloque algum texto;
 
 // criar o elemento p
-let newP = document.createElement('p');
+let paragraph = document.createElement('p');
 
 // por tag p como filho de section
-document.getElementsByTagName('section')[0].appendChild(newP);
+document.getElementsByTagName('section')[0].appendChild(paragraph);
+
+// adicionando texto
+paragraph.innerHTML = "Texto teste e vai pegar de primeira confia";
 
 // 5 - Adicione a tag section com a classe left-content como filho da tag main criada no passo 2;
 
-// document.getElementsByClassName('center-content')[0].className += " left-content";
-document.getElementsByClassName('center-content')[0].classList.add("left-content");
+// criando a tag elementSectionLeft
+let elementSectionLeft = document.createElement('section');
+
+// atribuindo a classe left-content
+elementSectionLeft.className = 'left-content';
+
+// atribuindo sectionleft como filho da tag main
+elementMain.appendChild(elementSectionLeft);
 
 // 6 - Adicione a tag section com a classe right-content como filho da tag main criada no passo 2;
 
+// criando o elemento sectionright
+let elementSectionRight = document.createElement('section');
 
+// atribuindo a classe right-content
+elementSectionRight.className = 'right-content';
+
+// atribuindo sectionright como filha da tag main
+elementMain.appendChild(elementSectionRight);
 
 // 7 - Adicione uma imagem com src configurado para o valor https://picsum.photos/200 e classe small-image . Esse elemento deve ser filho do section criado no passo 5;
 
+//criando elemento img
+let newImg = document.createElement('img');
+
+
+//anexando atributo src na img
+newImg.src = "https://picsum.photos/200";
+
+//atribuindo a classe small-image
+newImg.classList.add("small-image");
+
+//atribuindo img como filho do section
+elementSectionLeft.appendChild(newImg);
 
 // 8 - Adicione uma lista não ordenada com os valores de 1 a 10 por extenso, ou seja, um , dois , três , ... como valores da lista. Essa lista deve ser filha do section criado no passo 6;
 
