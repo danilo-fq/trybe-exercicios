d1f1768710486753de61bca20ddb931ddb41fb39
Danilo <danilof.queiroz1@gmail.com>
2022-02-11 10:28:41 -0300

    exercicio 2.1 feito

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -107,3 +107,18 @@ for (let index = 1; index <= 3; index += 1) {
   elementH3.innerHTML = 'Esse daqui é ' + index;
   elementMain.appendChild(elementH3);
 }
+
+// Exercicios Parte 2
+
+// 2.1 - Adicione a classe title na tag h1 criada;
+
+document.getElementsByTagName('h1')[0].className = 'title';
+
+// 2.2 - Adicione a classe description nas 3 tags h3 criadas;
+
+
+
+// 2.3 - Remova a section criado no passo 5 (aquele que possui a classe left-content ). Utilize a função .removeChild() ;
+// 2.4 - Centralize a section criado no passo 6 (aquele que possui a classe right-content ). Dica: para centralizar, basta configurar o margin-right: auto da section ;
+// 2.5 - Troque a cor de fundo do elemento pai da section criada no passo 3 (aquela que possui a classe center-content ) para a cor verde;
+// 2.6 - Remova os dois últimos elementos ( nove e dez ) da lista criada no passo 8.
