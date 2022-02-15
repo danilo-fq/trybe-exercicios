8d74c7b78db9fb6672a2d977c29a86143e8f33ea
Danilo <danilof.queiroz1@gmail.com>
2022-02-15 10:59:08 -0300

    exercicio 1 feito

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -15,9 +15,25 @@ createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
 
-/* 1 - O array dezDaysList contém os dois últimos dias de novembro e os dias do mês de dezembro. Desenvolva uma função que crie dinamicamente cada dia do calendário e os adicione como filhos/filhas da tag <ul> com ID "days" . Note que os dias 29 e 30 de novembro estão no array pois representam respectivamente Domingo e Segunda-feira.
+/* 1 - O array dezDaysList contém os dois últimos dias de novembro e os dias do mês de dezembro. Desenvolva uma função que crie dinamicamente cada dia do calendário e os adicione como filhos/filhas da tag <ul> com ID "days" . Note que os dias 29 e 30 de novembro estão no array pois representam respectivamente Domingo e Segunda-feira.*/
 
-    1.1 - Os dias devem estar contidos em uma tag <li> , e todos devem ter a classe day . Ex: <li class="day">3</li>
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+// atribuir o pai a uma variável
+const daysList = document.querySelector('#days');
+
+//laço for para criar uma li atribuir o innerhtml e declarar que é filho de daysList
+function createDaysOfCalendar() {
+  for (let index = 0; index < dezDaysList.length; index += 1) {
+    const daysNumber = dezDaysList[index];
+    const daysListItem = document.createElement('li');
+    daysListItem.innerHTML = daysNumber;
+    daysList.appendChild(daysListItem); 
+  }
+}
+createDaysOfCalendar()
+
+    /*1.1 - Os dias devem estar contidos em uma tag <li> , e todos devem ter a classe day . Ex: <li class="day">3</li>
     1.2 - Os dias 24, 25 e 31 são feriados e, além da classe day , devem conter também a classe holiday . Ex: <li class="day holiday">24</li>
     1.3 - Os dias 4, 11, 18 e 25 são Sexta-feira. Eles devem conter a classe day e a classe friday . Ex: <li class="day friday">4</li> */
 
