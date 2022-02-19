a275e36c099ed8762ec5fa5cb9c1acfcd0f55277
Danilo <danilof.queiroz1@gmail.com>
2022-02-19 17:05:16 -0300

    exercicio bonus em andamento

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,5 +1,13 @@
 const submitButton = document.getElementById('send');
 
-submitButton.addEventListener('click', (event) => {
-  event.preventDefault();
-})
+  submitButton.addEventListener('click', (event) => {
+    const checkedInput2 = document.querySelector('#term-2').checked;
+    console.log(checkedInput2);
+    if (checkedInput2 === 'true') {
+      
+    } else {
+      event.preventDefault();
+      alert('Você não autorizou seus direitos de imagens');
+    } 
+  })
+
