6dd40de14aa5910fae2c378725ce2f3ae2b81304
Danilo Ferreira <danilof.queiroz1@gmail.com>
2022-03-06 22:51:51 -0300

    Exercicio 2 feito

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parte2.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parte2.js
@@ -1,6 +1,6 @@
 // EXERCICIO 1 - Crie uma função que receba um número e retorne seu fatorial.
 
-// Forma recursive tradiconal
+/* // Forma recursive tradiconal
 const factorialNumber = (number) => {
     let result = 1;
     if (number === 0 || number === 1) {
@@ -16,6 +16,17 @@ console.log(factorialNumber(5));
 // Com operador ternário de forma recursiva
 const factorialNumberRecursive = number => number === 0 ? 1 : number * factorialNumberRecursive(number - 1);
 
-console.log(factorialNumberRecursive(3));
+console.log(factorialNumberRecursive(3)); */
 
+// EXERCICIO 2 - Crie uma função que receba uma frase e retorne qual a maior palavra.
 
+const longestWord = phrase => {
+    const arrayWords = phrase.split(' ');
+    let result = '';
+    for (let index = 0; index < arrayWords.length; index += 1) {
+        arrayWords[index].length > result.length ? result = arrayWords[index] : result
+    }
+    return `a maior palavra é: ${result}!`;
+}
+
+console.log(longestWord('Antônio foi no banheiro e não sabemos o que aconteceu'));
