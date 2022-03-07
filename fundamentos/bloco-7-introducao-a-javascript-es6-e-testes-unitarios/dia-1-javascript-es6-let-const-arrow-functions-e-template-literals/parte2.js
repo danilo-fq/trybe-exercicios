b798ec633c1a8a4933621c9b5698eb99cda58e82
Danilo Ferreira <danilof.queiroz1@gmail.com>
2022-03-07 10:53:19 -0300

    funcao 2 do exercicio 4 feito

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parte2.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parte2.js
@@ -32,18 +32,35 @@ console.log(factorialNumberRecursive(3)); */
 console.log(longestWord('Antônio foi no banheiro e não sabemos o que aconteceu')); */
 
 // EXERCICIO 4 - Crie um código JavaScript com a seguinte especificação:
-                // Função 1 : Escreva uma função que vai receber uma string como parâmetro. Sua função deverá procurar pela letra x em uma string qualquer que você determinar e substituir pela string que você passou como parâmetro. Sua função deve retornar essa nova string .
+// Função 1 : Escreva uma função que vai receber uma string como parâmetro. Sua função deverá procurar pela letra x em uma string qualquer que você determinar e substituir pela string que você passou como parâmetro. Sua função deve retornar essa nova string .
 
-                // Um array com escopo global, que é o escopo do arquivo JS , nesse caso, contendo cinco strings com suas principais skills .
+// Um array com escopo global, que é o escopo do arquivo JS , nesse caso, contendo cinco strings com suas principais skills .
 
-                // Função 2 : Escreva uma função que vai receber a string retornada da Função 1 como parâmetro. Essa função deve concatenar as skills do array global à string que foi passada para a Função 2 via parâmetro. Você deve ordenar os skills em ordem alfabética. Sua função deve retornar essa nova string .
+// Função 2 : Escreva uma função que vai receber a string retornada da Função 1 como parâmetro. Essa função deve concatenar as skills do array global à string que foi passada para a Função 2 via parâmetro. Você deve ordenar os skills em ordem alfabética. Sua função deve retornar essa nova string .
 
-const searchXLetter = word => {
-    let string = 'Tryber x aqui!';
-    let result;
-    if (string.includes('x')) {
-       result = string.replace('x', word);
-    }
-    return result;
-}
-console.log(searchXLetter('Danilo'));
+const searchAndReplaceXLetter = (word) => {
+  let string = "Tryber x aqui!";
+  let result;
+  if (string.includes("x")) {
+    result = string.replace("x", word);
+  }
+  return result;
+};
+
+const skillsArray = ["Javascript", "Git", "Html", "Css", "Flexbox"];
+
+const concatSkills = () => {
+  const sortSkillsArray = skillsArray.sort();
+  let result = `${searchAndReplaceXLetter(
+    "Danilo"
+  )} Minhas cinco principais habilidades são:`;
+  for (const skill of sortSkillsArray) {
+    result += `
+    - ${skill}`;
+  }
+  return result += `
+    #goTrybe`;
+};
+
+console.log(concatSkills());
+
