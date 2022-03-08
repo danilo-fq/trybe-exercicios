cd3c2e72029da3aa2d4bc583ff4630891dffccff
Danilo Ferreira <danilof.queiroz1@gmail.com>
2022-03-08 10:45:50 -0300

    Exercicio 2 feito

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
@@ -1,17 +1,36 @@
-const sum = require('./sum');
+const {sum, myRemove} = require('./sum');
+
+//Exercicio 1
 
 describe('Exercise 1 - Tests', () => {
 
-  it('Check if the sum of 4 and 5 is 9.', () => {
+  it('1 - Check if the sum of 4 and 5 is 9.', () => {
     expect(sum(4, 5)).toBe(9);
-  })
+  });
 
-  it('Check if the sum of 0 and 0 is 0.', () => {
+  it('2 - Check if the sum of 0 and 0 is 0.', () => {
     expect(sum(0, 0)).toBe(0);
-  })
+  });
 
-  it('Check if the function sum show an error message when its using 4 and "5".', () => {
+  it('3&4 - Check if the function sum show an error message when its using 4 and "5".', () => {
     expect(() => {sum(4,'5')}).toThrow();
     expect(() => {sum(4,'5')}).toThrow('parameters must be numbers');
+  });
+})
+
+//Exercicio 2
+
+describe('Exercise 2 - Tests', () => {
+
+  it('1 - Verifica se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+  });
+
+  it('2 - Verifica se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+  });
+
+  it('3 - Verifica se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado', () => {
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
   })
 })
