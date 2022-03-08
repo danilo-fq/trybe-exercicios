ca5298f67669397dc2dc9382d764a81d34cd43a1
Danilo Ferreira <danilof.queiroz1@gmail.com>
2022-03-08 10:59:46 -0300

    Exercicio 3 feito

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
@@ -1,4 +1,4 @@
-const {sum, myRemove} = require('./sum');
+const {sum, myRemove, myFizzBuzz} = require('./sum');
 
 //Exercicio 1
 
@@ -32,5 +32,29 @@ describe('Exercise 2 - Tests', () => {
 
   it('3 - Verifica se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado', () => {
     expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+  });
+
+})
+
+describe('Exercise 3 - Tests', () => {
+
+  it('1 - Faça uma chamada com um número divisível por 3 e 5 e verifique se o retorno é o esperado', () => {
+    expect(myFizzBuzz(15)).toEqual('fizzbuzz');
+  });
+
+  it('2 - Faça uma chamada com um número divisível por 3 e verifique se o retorno é o esperado', () => {
+    expect(myFizzBuzz(6)).toEqual('fizz');
+  })
+
+  it('3 - Faça uma chamada com um número divisível por 5 e verifique se o retorno é o esperado', () => {
+    expect(myFizzBuzz(10)).toEqual('buzz');
+  })
+
+  it('4 - Faça uma chamada com um número que não é divisível por 3 ou 5 e verifique se o retorno é o esperado', () => {
+    expect(myFizzBuzz(2)).toEqual(2);
+  })
+
+  it('5 - Faça uma chamada com um parâmetro que não é um número e verifique se o retorno é o esperado', () => {
+    expect(myFizzBuzz('a')).toEqual(false);
   })
 })
