838bdc01f2bec4ff1deb7a918b54211dac85261f
Danilo Ferreira <danilof.queiroz1@gmail.com>
2022-03-09 19:08:40 -0300

    Exercicio 2 feito

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
@@ -25,5 +25,27 @@ const register = (nomeCompleto) => {
   return { nome: nomeCompleto, email: `${newNameFormat}@trybe.com` }
 }
 
+// EXERCICIO 2 - Desenvolva uma HOF que retorna o resultado de um sorteio. Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string (Ex: "Tente novamente" ou "Parabéns você ganhou").
+
+//baby steps
+// 1 - Criar uma HOF que retorna o resultado de um sorteio (se ganhou ou nao)
+// 2 - gerá um número aleatório entre 1 e 5
+// 3 - a HOF retorna uma string ("Tente novamente" ou "Parabéns você ganhou")
+// 3 - tem como parametroS um número e uma callback function
+// 4 - a callback function checa se o número é igual ao sorteado
+// 5 - Como sortear um número entre 1 e 5? (Math.floor(Math.random() * 5) + 1)
+
+//ref: https://www.codegrepper.com/code-examples/javascript/random+number+between+1+and+5+javascript
+
+const raffle = (number, checkRaffle) => {
+  let result;
+  const drawnNumber = Math.floor(Math.random() * 5) + 1;
+  checkRaffle(number, drawnNumber) ? result = "Parabéns você ganhou" : result = "Tente novamente";
+  
+  return `${result}!! Seu número foi ${number} e o número sorteado ${drawnNumber}`;
+}
+
+const checkRaffle = (number, drawnNumber) => drawnNumber === number;
 
 
+console.log(raffle(5, checkRaffle));
