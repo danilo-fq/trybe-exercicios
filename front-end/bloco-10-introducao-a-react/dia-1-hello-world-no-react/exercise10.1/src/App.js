246e6c61cc1ee9c0c85b2e0f42ef37b787a10862
Danilo Ferreira <danilof.queiroz1@gmail.com>
2022-04-04 19:02:10 -0300

    Exercicios 1 e 2

--- front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercise10.1/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercise10.1/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+// Exercicio 1 - Crie um novo projeto utilizando npx create-react-app nome-app e acesse a pasta nome-app (nome que desejar)
+
+/* Exercicio 2 - Crie uma lista de tarefas simples seguindo os passos abaixo:
+
+ - insira a função Task a seguir acima do seu   componente App dentro do arquivo App.js
+
+ - agora, chame a função dentro do seu componente App (não se esqueça da sintaxe JSX!).Insira o valor que você quiser, salve a página e inicie-a rodando o comando npm start.
+ 
+ - por fim, crie uma array de compromissos e use a função map para que cada item do array apareça, como um item de lista, no seu componente App . */
+
+
+// import logo from './logo.svg';
 import './App.css';
+
+const Task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+}
+
+const tasks = ['Estudar React', 'Fazer Resumos', 'Treinar BJJ', 'Ler um livro']
+
+const mapTasks = tasks.map((task) => Task(task))
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {mapTasks}
+      </ul>
     </div>
   );
 }
